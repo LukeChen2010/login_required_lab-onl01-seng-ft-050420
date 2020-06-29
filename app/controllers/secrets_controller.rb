29a14fd6cd4ba158ef51e12553baa92d0baf7d1e
Luke Chen <luke.chen.2010@gmail.com>
2020-06-29 21:17:31 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < ApplicationController
   before_action :require_logged_in
   
-  def show
+  def create
     if !params[:name] || params[:name] == ""
       redirect_to(controller: 'sessions', action: 'new')
     else
